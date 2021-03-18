bd7a22d077441dfb42850049c453615771160c67
Thomas Marrinan <thomas.j.marrinan@gmail.com>
2021-03-18 09:37:18 -0500

    remove old code

--- include/paricompress.h
+++ include/paricompress.h
@@ -38,13 +38,6 @@ PARI_DLLEXPORT void pariGetRgbaTextureAsDxt1(PariCGResource cg_resource, PariCGR
                                              uint8_t *dxt1);
 
 
-//void initImageConverter(int width, int height);
-//void rgbaToGrayscale(uint8_t *rgba, uint8_t *gray);
-//void rgbaToDxt1(uint8_t *rgba, uint8_t *dxt1);
-//void rgbaToTrle(uint8_t *rgba, uint8_t *trle, uint32_t *buffer_size, uint32_t *run_offsets);
-//void finalizeImageConverter();
-//void trleToRgb(uint8_t *trle, uint8_t *rgb, uint32_t buffer_size, uint32_t *run_offsets);
-
 #ifdef __cplusplus
 }
 #endif
